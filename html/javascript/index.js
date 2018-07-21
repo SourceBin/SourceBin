8d48346811961ec884e7251fe44b61b5345ddb86
SebastiaanYN <sebastiaanyn@gmail.com>
2018-07-21 19:13:34 +0200

    Added a box that pops up after saving with the link of the bin

--- html/javascript/index.js
+++ html/javascript/index.js
@@ -11,7 +11,8 @@ function Bin(options, languages) {
   editor.setReadOnly(readOnly);
   editor.setFontSize(15);
   if (editor.getValue() === 'Loading...') editor.setValue('', -1);
-  this.LanguageSelector = new LanguageSelector(this, editor);
+  this.LanguageSelector = new LanguageSelector(this);
+  this.LinkBox = new LinkBox(this);
 
   this.focus = () => editor.focus();
   this.setReadOnly = boolean => editor.setReadOnly(boolean);
@@ -22,39 +23,55 @@ function Bin(options, languages) {
       if (language) url += '.' + language.extension;
     }
     return window.history.pushState(null, null, url);
-  }
+  };
   this.setLanguage = lang => {
     if (lang) language = lang;
     if (!language) return;
     document.getElementById('lang').innerHTML = `Language - ${language.name}`;
     return editor.session.setMode(`ace/mode/${language.ace}`);
-  }
+  };
   this.save = () => {
     if (this.isDisabled('Save')) return;
     this.disableSave();
     editor.setReadOnly(true);
-    this.LanguageSelector.show();
+    this.LanguageSelector.show(true);
     return _request('post', '/', editor.getValue())
       .then(response => {
         if (response) key = response.key;
         return this.setURL();
       });
-  }
+  };
   this.disableSave = () => {
     const save = document.getElementById('save');
     const className = save.className;
     save.className += className.includes('disabled') ? '' : ' disabled';
     return disabled.add('Save');
-  }
+  };
 
   this.setURL();
   this.setLanguage();
   if (!options.saving) this.disableSave();
 }
 
-function LanguageSelector(bin, editor) {
-  const input = document.getElementById('search');
+function LinkBox(bin) {
+  const box = document.getElementById('linkbox');
+  const textarea = document.getElementById('link');
+  this.set = string => textarea.innerHTML = string;
+  this.show = () => {
+    this.set(window.location.href);
+    box.setAttribute('style', 'display:inherit');
+    return setTimeout(() => textarea.select(), 100);
+  };
+  this.hide = () => {
+    box.setAttribute('style', 'display:none');
+    return bin.focus();
+  };
+}
 
+function LanguageSelector(bin) {
+  let showLink = false;
+
+  const input = document.getElementById('search');
   const addLanguages = () => {
     const search = input.value.toLowerCase();
     const options = document.getElementById('options');
@@ -71,21 +88,24 @@ function LanguageSelector(bin, editor) {
           if (!response) return;
           bin.setLanguage(response);
           bin.setURL();
-          return this.hide();
+          this.hide();
+          if (showLink) bin.LinkBox.show();
+          return;
         });
       });
     }
-  }
+  };
   input.addEventListener('input', addLanguages);
 
-  this.show = () => {
+  this.show = link => {
+    showLink = link ? true : false;
     document.getElementById('language').setAttribute('style', 'display:inherit');
     addLanguages();
     return input.focus();
-  }
+  };
 
   this.hide = () => {
     document.getElementById('language').setAttribute('style', 'display:none');
-    return editor.focus();
-  }
+    return bin.focus();
+  };
 }
