9a91b17f3332da338ec4c357f8e974c13303ebff
SebastiaanYN <sebastiaanyn@gmail.com>
2018-12-28 21:55:54 +0100

    Fixed a bug in the bin model

--- models/Bin.js
+++ models/Bin.js
@@ -4,6 +4,6 @@ const Model = new mongoose.Schema({
   id: String,
   key: String,
   code: String,
-  created: { type: Date, default: new Date() },
+  created: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model('bin', Model);
