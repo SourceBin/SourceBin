b9e3246f4b06d9241ade05001ad0d36346ba0298
SebastiaanYN <sebastiaanyn@gmail.com>
2020-01-12 14:55:21 +0100

    Replace language with languageId

--- backend/src/controllers/bins/getBin.ts
+++ backend/src/controllers/bins/getBin.ts
@@ -9,7 +9,7 @@ export async function getBin(req: Request, res: Response): Promise<void> {
 
   const bin = await Bin
     .findOne({ key })
-    .select('-_id key content language created')
+    .select('-_id key content languageId created')
     .exec();
 
   if (bin) {
