6badd80650b297910c89a56533e26757df9047a3
SebastiaanYN <sebastiaanyn@gmail.com>
2020-01-26 13:53:50 +0100

    Setup nginx

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -eu
+
+# All templates end with .conf.template
+TEMPLATES=/etc/nginx/conf.d/*.conf.template
+
+# Loop over all templates
+for TEMPLATE in $TEMPLATES
+do
+  # Replace the environment variables in the templates and output the config
+  envsubst '${DOMAIN} ${FRONTEND_URL} ${BACKEND_URL}' \
+    < "$TEMPLATE" \
+    > "${TEMPLATE%.*}"
+done
+
+# Execute command
+exec "$@"
