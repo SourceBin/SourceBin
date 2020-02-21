79411c95b50d5d9e477ccfab7c4c71fb6ee79343
SebastiaanYN <sebastiaanyn@gmail.com>
2020-02-21 17:18:11 +0100

    Reset bin when clicking plus, don't reset if no bin found

--- frontend/assets/home/loadBin.js
+++ frontend/assets/home/loadBin.js
@@ -4,8 +4,6 @@ export async function loadBin(route, store, error) {
       await store.dispatch('bin/loadFromKey', route.params.key);
     } else if (route.query.src) {
       await store.dispatch('bin/loadFromQuery', route.query);
-    } else {
-      store.commit('bin/reset');
     }
   } catch (err) {
     error({
