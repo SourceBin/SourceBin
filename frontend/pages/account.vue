b4305df1e09c6dc7934617450949dccea981420c
SebastiaanYN <sebastiaanyn@gmail.com>
2020-02-23 13:38:55 +0100

    Improve error page, add coming soon account page

---
+++ frontend/pages/account.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <h1>Coming Soon!</h1>
+</template>
+
+<style lang="scss" scoped>
+@import 'sass-mq';
+@import '@/assets/_globals.scss';
+
+$font-size: 50px;
+
+h1 {
+  margin: 25px $margin-side;
+  font-family: $font-family;
+  font-size: $font-size;
+  color: $white;
+  opacity: 90%;
+
+  @include mq($until: desktop) {
+    margin: 25px $margin-side-small;
+  }
+}
+</style>
