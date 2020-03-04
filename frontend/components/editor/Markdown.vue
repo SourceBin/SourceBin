3534d0ac95714555e6b226eab7ceb7bb0e05766e
SebastiaanYN <sebastiaanyn@gmail.com>
2020-03-04 17:40:51 +0100

    feat(editor): add button to display rendered Markdown

---
+++ frontend/components/editor/Markdown.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+  <div class="markdown-body" />
+</template>
+
+<script>
+import { render } from '@/assets/markdown/markdown.js';
+
+export default {
+  props: {
+    markdown: {
+      type: String,
+      required: true,
+    },
+  },
+  watch: {
+    markdown() {
+      this.render();
+    },
+  },
+  mounted() {
+    this.render();
+  },
+  methods: {
+    async render() {
+      this.$el.innerHTML = await render(this.markdown);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import 'sass-mq';
+@import 'github-markdown-css';
+@import 'highlight.js/styles/atom-one-dark.css';
+@import '@/assets/_globals.scss';
+
+$hljs-theme-background: #282c34;
+
+.markdown-body {
+  background-color: $white;
+  padding: 45px;
+
+  @include mq($until: tablet) {
+    padding: 15px;
+  }
+}
+
+.markdown-body pre,
+.markdown-body .highlight pre {
+  background-color: $hljs-theme-background;
+}
+</style>
