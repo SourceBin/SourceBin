ee6c15c2d1c573e143debd1928ea7a5ffad92990
SebastiaanYN <sebastiaanyn@gmail.com>
2020-03-05 21:28:25 +0100

    feat(nginx): proxy_pass /proxy/ to the proxy container

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -8,7 +8,7 @@ TEMPLATES=/etc/nginx/conf.d/*.conf.template
 for TEMPLATE in $TEMPLATES
 do
   # Replace the environment variables in the templates and output the config
-  envsubst '${DOMAIN} ${FRONTEND_URL} ${BACKEND_URL}' \
+  envsubst '${DOMAIN} ${FRONTEND_URL} ${BACKEND_URL} ${PROXY_URL}' \
     < "$TEMPLATE" \
     > "${TEMPLATE%.*}"
 done
