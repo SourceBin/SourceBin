8923484c5fa2c0b83a6d0e225205140584ef4b1d
SebastiaanYN <sebastiaanyn@gmail.com>
2020-04-01 18:05:20 +0200

    feat(accounts): redirect users to /accounts?error=... when authentication fails

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,23 +1,17 @@
 import { Router } from 'express';
-import passport from 'passport';
+
+import { registerOrLogin } from '../middleware/registerOrLogin';
 
 import { authenticated } from '../controllers/auth/authenticated';
 import { logout } from '../controllers/auth/logout';
 
-function authenticate(strategy: string): any {
-  return passport.authenticate(strategy, {
-    session: false,
-    failureRedirect: '/account',
-  });
-}
-
 const router = Router();
 
-router.get('/discord', authenticate('discord'));
-router.get('/discord/callback', authenticate('discord'), authenticated);
+router.get('/discord', registerOrLogin('discord'));
+router.get('/discord/callback', registerOrLogin('discord'), authenticated);
 
-router.get('/github', authenticate('github'));
-router.get('/github/callback', authenticate('github'), authenticated);
+router.get('/github', registerOrLogin('github'));
+router.get('/github/callback', registerOrLogin('github'), authenticated);
 
 router.get('/logout', logout);
 
