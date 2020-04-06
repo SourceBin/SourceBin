a56cb0b2ad36607e877b85c0955aada8fcbe9701
SebastiaanYN <sebastiaanyn@gmail.com>
2020-04-06 18:00:49 +0200

    feat(accounts): display language color

--- frontend/pages/account.vue
+++ frontend/pages/account.vue
@@ -62,38 +62,50 @@ export default {
   display: flex;
   align-items: flex-start;
   align-self: center;
-  margin: 0 $margin-side 15px;
+  margin: 0 $margin-side;
+  padding-bottom: 15px;
   font-family: $font-family;
 
-  @include mq($until: tablet) {
+  @include mq($until: desktop) {
     flex-direction: column;
+    margin: 0 $margin-side-small;
   }
 }
 
 .about {
-  margin-right: 75px;
+  margin: 0 75px 25px 0;
 
   @include mq($until: tablet) {
     margin-right: 0;
+    display: flex;
   }
 
   .avatar {
-    width: 300px;
+    width: 100%;
+    height: 100%;
+    max-width: 300px;
+    margin-bottom: 10px;
+
+    @include mq($until: tablet) {
+      width: 75px;
+      margin-bottom: 0;
+      margin-right: 10px;
+    }
   }
 
   .username {
-    margin: 10px 0;
-    color: rgba($white, 0.8);
+    margin: 0;
+    color: rgba($white, 0.7);
   }
 }
 
 .bins {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(2, minmax(200px, 400px));
   grid-gap: 20px;
 
-  @include mq($until: desktop) {
-    grid-template-columns: 1fr;
+  @include mq($until: tablet) {
+    grid-template-columns: minmax(200px, 400px);
   }
 }
 </style>
