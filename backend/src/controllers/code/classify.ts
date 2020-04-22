6a5f7b52640f3ed3c5144b401dc2d4c8dfcb1cb4
SebastiaanYN <sebastiaanyn@gmail.com>
2020-04-22 19:16:57 +0200

    feat: handle axios error in classify

--- backend/src/controllers/code/classify.ts
+++ backend/src/controllers/code/classify.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import Joi from '@hapi/joi';
 import axios from 'axios';
 
-import { replyJoiError } from '../../utils/errors';
+import { replyError, replyJoiError } from '../../utils/errors';
 
 const schema = Joi.array()
   .required()
@@ -19,6 +19,10 @@ export async function classify(req: Request, res: Response): Promise<void> {
     return;
   }
 
-  const { data } = await axios.post(`http://${process.env.GENUS_CODICE_URL}/classify`, req.body);
-  res.json(data);
+  try {
+    const { data } = await axios.post(`http://${process.env.GENUS_CODICE_URL}/classify`, req.body);
+    res.json(data);
+  } catch {
+    replyError(500, 'Failed to classify code', res);
+  }
 }
