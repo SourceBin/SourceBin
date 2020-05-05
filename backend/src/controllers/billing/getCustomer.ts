ec2947c03fcff0fe2c7b9609794a93df98c62323
SebastiaanYN <sebastiaanyn@gmail.com>
2020-05-05 22:27:17 +0200

    feat: simplify checkout flow

--- backend/src/controllers/billing/getCustomer.ts
+++ backend/src/controllers/billing/getCustomer.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from 'express';
 
 import { replyError } from '../../utils/errors';
-import { createOrGetCustomer } from '../../utils/stripe';
+import { getCustomer as getStripeCustomer } from '../../utils/stripe';
 
 export async function getCustomer(req: Request, res: Response): Promise<void> {
   if (!req.user) {
     throw new Error('User unavailable after authentication');
   }
 
+  if (!req.user.subscription.stripeId) {
+    replyError(400, 'No customer', res);
+    return;
+  }
+
   try {
-    const customer = await createOrGetCustomer(req.user);
+    const customer = await getStripeCustomer(req.user.subscription.stripeId);
 
     res.json(customer);
   } catch (err) {
     console.error(err);
 
-    replyError(500, 'Error fetching customer', res);
+    replyError(400, err.message, res);
   }
 }
