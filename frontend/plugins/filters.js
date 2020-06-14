70e0078b5881b4046d3403eba4f454c49375c6fb
SebastiaanYN <sebastiaanyn@gmail.com>
2020-06-14 17:44:44 +0200

    fix: return Today in date filter if no date provided

--- frontend/plugins/filters.js
+++ frontend/plugins/filters.js
@@ -1,9 +1,23 @@
 import Vue from 'vue';
 
 Vue.filter('capitalize', s => s.charAt(0).toUpperCase() + s.slice(1));
+
 Vue.filter('currency', c => `$${(c / 100).toFixed(2)}`);
-Vue.filter('date', d => (d instanceof Date ? d : new Date(d)).toLocaleString(undefined, {
-  month: 'short',
-  day: 'numeric',
-  year: 'numeric',
-}));
+
+Vue.filter('date', (d) => {
+  if (!d) {
+    return 'Today';
+  }
+
+  const date = d instanceof Date ? d : new Date(d);
+
+  if (date.toDateString() === new Date().toDateString()) {
+    return 'Today';
+  }
+
+  return date.toLocaleString(undefined, {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+});
