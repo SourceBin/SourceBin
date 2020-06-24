796a27325b616eb446c8f9b37653436b19b182da
SebastiaanYN <sebastiaanyn@gmail.com>
2020-06-24 14:57:22 +0200

    refactor: pass query param to redirect instead of encodeURIComponent

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -1,5 +1,7 @@
 export default function ({ store, redirect, route }) {
   if (!store.state.auth.loggedIn) {
-    redirect(`/login?redirect=${encodeURIComponent(route.fullPath)}`);
+    redirect('/login', {
+      redirect: route.fullPath,
+    });
   }
 }
